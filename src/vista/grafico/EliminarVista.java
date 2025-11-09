e0385d1a95ef8ac5de58225bf87bbbbcbb7903fd
Dilan Rojas <rojasvargasdilan010@gmail.com>
2025-11-09 15:01:14 -0600

    vistas

--- src/vista/grafico/EliminarVista.java
+++ src/vista/grafico/EliminarVista.java
@@ -1,12 +1,85 @@
 package vista.grafico;
 
-/**
- * @author Dilan Rojas
- * @date Nov 5, 2025
- * @version 1.0
- * @description description
- */
+import java.awt.event.ActionListener;
 
-public class EliminarVista {
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 
+
+import javax.swing.JButton;
+import java.awt.Color;
+import java.awt.EventQueue;
+
+public class EliminarVista extends JFrame{
+
+	private static final long serialVersionUID = 1L;
+	private JLabel lblUsuario;
+	private JTextField txtUsuario;
+	private JButton btnBuscar;
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					EliminarVista frame = new EliminarVista();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	
+	public EliminarVista() {
+		getContentPane().setBackground(new Color(240, 128, 128));
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setTitle("\\\\\\Eliminar Usuario///");
+		setSize(400, 250);
+		setResizable(true);
+		setLocationRelativeTo(null);
+		
+		initComponent();
+		}
+	
+	public void initComponent() {
+		getContentPane().setLayout(null);
+		
+		lblUsuario = new JLabel("BUSCAR USUARIO");
+		lblUsuario.setHorizontalAlignment(SwingConstants.CENTER);
+		lblUsuario.setBounds(140, 40, 120, 25);
+		getContentPane().add(lblUsuario);
+		
+		txtUsuario = new JTextField();
+		txtUsuario.setHorizontalAlignment(SwingConstants.CENTER);
+		txtUsuario.setText("Nombre de usuario");
+		txtUsuario.setBounds(100, 80, 200, 30);
+		getContentPane().add(txtUsuario);
+		txtUsuario.setColumns(10);
+		
+		btnBuscar = new JButton("Buscar");
+		btnBuscar.setBounds(150, 130, 100, 25);
+		getContentPane().add(btnBuscar);
+	}
+	
+	public JTextField getTxtUsuario() {
+		return txtUsuario;
+	}
+	
+	public void cerrar() {
+		dispose();
+	}
+	
+	public void mostrarMensaje(String msg) {
+		JOptionPane.showMessageDialog(this, msg);
+		
+	}
+	
+	public void actionListener(ActionListener listener) {
+		txtUsuario.addActionListener(listener);
+	}
+
+	
 }
--- src/vista/grafico/InsertarVista.java
+++ src/vista/grafico/InsertarVista.java
@@ -1,23 +1,47 @@
 package vista.grafico;
 
-import java.awt.EventQueue;
+import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+
+
+import javax.swing.JButton;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.EventQueue;
+import java.awt.Font;
 
 /**
- * @author Dilan Rojas
+ * @author Sebastian Castro Ulate
  * @date Nov 5, 2025
  * @version 1.0
  * @description description
  */
 
 public class InsertarVista extends JFrame {
-
+	
+	
 	private static final long serialVersionUID = 1L;
-	private JPanel contentPane;
-
+	private JTextField txtNickname;
+	private JTextField txtContrasena;
+	private JTextField txtGameLevel;
+	private JLabel lblNickname;
+	private JLabel lblContrasena;
+	private JLabel lblPuntaje1;
+	private JLabel lblPuntaje2;
+	private JLabel lblGameLevel;
+	private JLabel lblID;
+	private JLabel lblIDIndicator;
+	private JButton btnCrear;
+	
+	
+	
+	
+	
 	/**
 	 * Launch the application.
 	 */
@@ -33,17 +57,132 @@ public class InsertarVista extends JFrame {
 			}
 		});
 	}
-
-	/**
-	 * Create the frame.
-	 */
 	public InsertarVista() {
+		getContentPane().setBackground(new Color(153, 193, 241));
+//		Cerrar app
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
+		setTitle("[CREAR USUARIO]");
+		setSize(475, 300);
+		setResizable(true);
+		setLocationRelativeTo(null);
+		
+		initComponent();
+		
+		setPreferredSize(new Dimension(475, 300));
+		pack();
+	}
+	
+	
+	public void initComponent() {
+		getContentPane().setLayout(null);
+		
+		
+		lblNickname = new JLabel("Ingrese Nickname");
+		lblNickname.setBounds(183, 40, 110, 17);
+		lblNickname.setHorizontalAlignment(SwingConstants.CENTER);
+		getContentPane().add(lblNickname);
+		
+		txtNickname = new JTextField();
+		txtNickname.setForeground(new Color(255, 255, 255));
+		txtNickname.setBackground(new Color(26, 95, 180));
+		txtNickname.setBounds(183, 60, 110, 30);
+		txtNickname.setHorizontalAlignment(SwingConstants.CENTER);
+		getContentPane().add(txtNickname);
+		//txtNickname.setColumns(10);
+		
+		lblContrasena = new JLabel("Contraseña");
+		lblContrasena.setBounds(183, 100, 110, 17);
+		lblContrasena.setHorizontalAlignment(SwingConstants.CENTER);
+		getContentPane().add(lblContrasena);
+		
+		txtContrasena = new JTextField();
+		txtContrasena.setForeground(new Color(255, 255, 255));
+		txtContrasena.setBackground(new Color(26, 95, 180));
+		txtContrasena.setBounds(183, 120, 110, 30);
+		txtContrasena.setHorizontalAlignment(SwingConstants.CENTER);
+		getContentPane().add(txtContrasena);
+		txtContrasena.setColumns(10);
+		
+		lblPuntaje1 = new JLabel("Puntaje");
+		lblPuntaje1.setBounds(183, 160, 110, 17);
+		lblPuntaje1.setHorizontalAlignment(SwingConstants.CENTER);
+		getContentPane().add(lblPuntaje1);
+		
+		lblPuntaje2 = new JLabel("{{0}}");
+		lblPuntaje2.setBackground(new Color(255, 230, 0));
+		lblPuntaje2.setHorizontalAlignment(SwingConstants.CENTER);
+		lblPuntaje2.setBounds(183, 180, 110, 30);
+		getContentPane().add(lblPuntaje2);
+		
+		lblGameLevel = new JLabel("/Nivel de juego\\");
+		lblGameLevel.setBounds(51, 50, 100, 20);
+		lblGameLevel.setHorizontalAlignment(SwingConstants.CENTER);
+		getContentPane().add(lblGameLevel);
+		
+		txtGameLevel = new JTextField();
+		txtGameLevel.setForeground(new Color(255, 255, 255));
+		txtGameLevel.setBackground(new Color(35, 39, 80));
+		txtGameLevel.setFont(new Font("Dialog", Font.PLAIN, 30));
+		txtGameLevel.setBounds(51, 80, 100, 100);
+		txtGameLevel.setHorizontalAlignment(SwingConstants.CENTER);
+		getContentPane().add(txtGameLevel);
+		txtGameLevel.setColumns(10);
+		
+		lblID = new JLabel("ID:");
+		lblID.setBounds(340, 240, 40, 10);
+		lblID.setHorizontalAlignment(SwingConstants.CENTER);
+		getContentPane().add(lblID);
+		
+		lblIDIndicator = new JLabel("-Numeros-");
+		lblIDIndicator.setBounds(380, 240, 80, 10);
+		lblIDIndicator.setHorizontalAlignment(SwingConstants.CENTER);
+		getContentPane().add(lblIDIndicator);
+		
+		btnCrear = new JButton("Crear");
+		btnCrear.setFont(new Font("Dialog", Font.BOLD, 24));
+		btnCrear.setForeground(new Color(255, 255, 255));
+		btnCrear.setBackground(new Color(35, 39, 80));
+		btnCrear.setBounds(340, 80, 100, 100);
+		getContentPane().add(btnCrear);
+		
+		
 
 	}
 
+	public JTextField getTxtNickname() {
+		return txtNickname;
+	}
+	
+	public JTextField getTxtContrasena() {
+		return txtContrasena;
+	}
+
+	public JTextField getTxtGameLevel() {
+		return txtGameLevel;
+	}
+	
+	public void setLblIDIndicator(String ID) {
+		lblIDIndicator = new JLabel("{" + ID +"}");
+	}
+	
+	public void setLblPuntaje2(int puntaje) {
+		lblPuntaje2 = new JLabel("{{"+puntaje+"}}");
+	}
+
+
+	public void cerrar() {
+		dispose();
+	}
+	
+	public void mostrarMsj(String msg) {
+	      JOptionPane.showMessageDialog(this, msg);
+	  }
+	
+	public void setActionListener(ActionListener listener) {
+		txtNickname.addActionListener(listener);
+		txtContrasena.addActionListener(listener);
+		txtGameLevel.addActionListener(listener);
+		btnCrear.addActionListener(listener);
+		
+	}
 }
