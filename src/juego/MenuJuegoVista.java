6102f1046a7e6e6612f13b98c5f66178190009d3
Ana <silva05anagonzalez@gmail.com>
2025-11-19 23:19:58 -0600

    Update MenuJuegoVista.java

--- src/juego/MenuJuegoVista.java
+++ src/juego/MenuJuegoVista.java
@@ -1,10 +1,13 @@
 package juego.vista.grafica;
 
-import java.awt.EventQueue;
-
 import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
+
+import javax.swing.JButton;
+import java.awt.Font;
+import java.awt.event.ActionListener;
+
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 
 /**
  * @author AnaGonzalezC5F593
@@ -16,20 +19,77 @@ import javax.swing.border.EmptyBorder;
 public class MenuJuegoVista extends JFrame {
 
 	private static final long serialVersionUID = 1L;
+	private JLabel lblMenu;
+	private JButton btnEstadisticas;
+	private JButton btnSalir;
+	private JButton btnJugar;
+	
+	// Main (Pruebas)
+	public static void main(String[] args) {
+		MenuJuegoVista vista = new MenuJuegoVista();
+	}
 
 	public MenuJuegoVista() {
 		initComponents();
-        setTitle("Menu de juego");
-        setSize(500, 500);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setResizable(false);
-        setLocationRelativeTo(null);
-        getContentPane().setLayout(null);
-        setVisible(true);
+		setTitle("Menu de juego");
+		setSize(400, 350);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setResizable(false);
+		setLocationRelativeTo(null);
+		getContentPane().setLayout(null);
+		setVisible(true);
 	}
+
 	private void initComponents() {
-		
+		btnJugar = new JButton("Jugar");
+		btnJugar.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		btnJugar.setBounds(124, 88, 122, 37);
+		getContentPane().add(btnJugar);
+
+		btnEstadisticas = new JButton("Estadisticas");
+		btnEstadisticas.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		btnEstadisticas.setBounds(124, 147, 122, 37);
+		getContentPane().add(btnEstadisticas);
+
+		btnSalir = new JButton("Salir");
+		btnSalir.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		btnSalir.setBounds(124, 203, 122, 37);
+		getContentPane().add(btnSalir);
+
+		lblMenu = new JLabel("Menu del jugador");
+		lblMenu.setFont(new Font("Tahoma", Font.BOLD, 13));
+		lblMenu.setBounds(124, 30, 146, 26);
+		getContentPane().add(lblMenu);
+	}
+
+	//Getters
+	public JButton getBtnEstadisticas() {
+		return btnEstadisticas;
+	}
+
+	public JButton getBtnSalir() {
+		return btnSalir;
+	}
+
+	public JButton getBtnJugar() {
+		return btnJugar;
+	}
+
+	// Configurar escuchadores
+	public void setEscuchadores(ActionListener listener) {
+		btnJugar.addActionListener(listener);
+		btnEstadisticas.addActionListener(listener);
+		btnSalir.addActionListener(listener);
+	}
+
+	// Método mostrar mensaje
+	public void mostrarMsj(String msg) {
+		JOptionPane.showMessageDialog(this, msg, "Menu Juego", JOptionPane.INFORMATION_MESSAGE);
+	}
+
+	// Método cerrar
+	public void cerrar() {
+		dispose();
 	}
 
 }
-
