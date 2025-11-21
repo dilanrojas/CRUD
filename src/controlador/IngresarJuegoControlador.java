d705fe9c801cc47f9e9e491653970015d4591a36
Dilan Rojas <rojasvargasdilan010@gmail.com>
2025-11-20 18:30:25 -0600

    Paquetes

---
+++ src/controlador/IngresarJuegoControlador.java
@@ -0,0 +1,23 @@
+package controlador;
+
+import CRUD.modelo.dao.IUsuarioDAO;
+import vista.IngresarJuegoVista;
+
+/**
+ * @author AnaGonzalezC5F593
+ * @date 19 nov 2025
+ * @version 1.0
+ * @description TODO
+ */
+
+public class IngresarJuegoControlador {
+	private IngresarJuegoVista vista;
+	private IUsuarioDAO modelo;
+	
+	public IngresarJuegoControlador(IngresarJuegoVista vista, IUsuarioDAO modelo) {
+		this.vista = vista;
+		this.modelo = modelo;
+	}
+
+}
+
---
+++ src/principal/AppGeometryWars.java
@@ -0,0 +1,35 @@
+package principal;
+
+import CRUD.modelo.dao.IUsuarioDAO;
+import CRUD.modelo.dao.archivoJSON.UsuarioDAO;
+import CRUD.modelo.dataset.ListaUsuarios;
+import controlador.BienvenidaControlador;
+import vista.BienvenidaVista;
+
+/**
+ * @author Dilan Rojas
+ * @date Nov 20, 2025
+ * @version 1.0
+ * @description description
+ */
+
+public class AppGeometryWars {
+	// Componentes
+	private BienvenidaVista bienvenidaVista;
+	private BienvenidaControlador bienvenidaControlador;
+	private ListaUsuarios dataset;
+	
+	// Constructor
+	public static void main(String[] args) {
+		AppGeometryWars app = new AppGeometryWars();
+		app.iniciar();
+	}
+	
+	public void iniciar() {
+		bienvenidaVista = new BienvenidaVista();
+		dataset = new ListaUsuarios();
+		UsuarioDAO modelo = new UsuarioDAO(dataset);
+		bienvenidaControlador = new BienvenidaControlador(modelo, bienvenidaVista);
+	}
+	
+}
